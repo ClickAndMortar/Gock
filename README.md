de5d37e9775bbad4efc56b475a47ea08246787d3
michael-bouvy <michael.bouvy@gmail.com>
2020-07-15 16:10:06 +0200

    Add proxy mode

--- README.md
+++ README.md
@@ -8,6 +8,15 @@ Default response code is `204` (No Content).
 
 Query params can be cumulated (ie. wait + code).
 
+Two modes are available:
+
+* `default`: Gock replies to queries directly
+* `proxy`: Gock proxifies queries to another backend
+
+```
+go run main.go
+```
+
 ### Request with wait (for timeout tests)
 
 Wait given amount of seconds before response:
@@ -23,3 +32,18 @@ Respond with `404` HTTP code:
 ```
 http://gock:8000/?code=404
 ```
+
+## Configuration
+
+Following environment variables may be set:
+
+| Variable | Description | Default value |
+|---|---|---|
+| `GOCK_PORT` | HTTP port to listen on | `8000` |
+| `GOCK_DEBUG` | Run in debug mode (set to `1`) | `0` |
+| `GOCK_MODE` | Gock mode: `default` or `proxy` | `default` |
+| `GOCK_PROXY_HOST` | Host to proxify in proxy mode (required) | _none_ |
+| `GOCK_PROXY_PORT` | Port to proxify in proxy mode (required) | `80` |
+| `GOCK_PROXY_CODE` | Response code in proxy mode | _none_ (backend code) |
+| `GOCK_PROXY_WAIT` | Waiting time in proxy mode (in seconds) | `0` |
+| `GOCK_PROXY_PERCENT` | Percentage (approximate) on which response code or waiting time apply | `100` |
--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"github.com/sirupsen/logrus"
+	"io"
+	"math/rand"
+	"net"
 	"net/http"
 	"os"
 	"strconv"
@@ -10,16 +13,87 @@ import (
 	"time"
 )
 
+var stdLog = logrus.New()
+var errLog = logrus.New()
+
 func main() {
-	http.HandleFunc("/", RequestHandler)
+	stdLog.Out = os.Stdout
+	debug := getEnvDefault("GOCK_DEBUG", "0")
+	if debug == "1" {
+		stdLog.SetLevel(logrus.DebugLevel)
+	}
+	errLog.Out = os.Stderr
 
 	port := getEnvDefault("GOCK_PORT", "8000")
 
-	log.Printf("Listening on http://0.0.0.0:%s", port)
+	mode := getEnvDefault("GOCK_MODE", "default")
+	if mode == "proxy" {
+		http.HandleFunc("/", ProxyRequestHandler)
+	} else {
+		http.HandleFunc("/", DefaultRequestHandler)
+	}
+
+	rand.Seed(time.Now().UnixNano())
+
+	stdLog.Infof("Listening on http://0.0.0.0:%s in %s mode", port, mode)
 	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 }
 
-func RequestHandler(writer http.ResponseWriter, request *http.Request) {
+func ProxyRequestHandler(writer http.ResponseWriter, request *http.Request) {
+	percent, _ := strconv.Atoi(getEnvDefault("GOCK_PROXY_PERCENT", "100"))
+	returnCode := 0
+
+	random := rand.Intn(100)
+	if random < percent {
+		stdLog.Debugf("Fault injection: random number %d < percentage %d", random, percent)
+		wait := getEnvDefault("GOCK_PROXY_WAIT", "0")
+		waitTime, _ := strconv.Atoi(wait)
+		duration, _ := time.ParseDuration(fmt.Sprintf("%ds", waitTime))
+		time.Sleep(duration)
+
+		code := getEnvDefault("GOCK_PROXY_CODE", "0")
+		if code != "" {
+			returnCode, _ = strconv.Atoi(code)
+		}
+	}
+
+	if request.URL.Scheme != "http" && request.URL.Scheme != "https" {
+		request.URL.Scheme = "http"
+	}
+
+	stdLog.Printf("%s %s", request.Method, strings.ReplaceAll(request.RequestURI, "%", "%%"))
+
+	client := &http.Client{}
+
+	request.URL.Host = fmt.Sprintf("%s:%s", getEnvDefault("GOCK_PROXY_HOST", ""), getEnvDefault("GOCK_PROXY_PORT", "80"))
+	request.RequestURI = ""
+
+	delHopHeaders(request.Header)
+
+	if clientIP, _, err := net.SplitHostPort(request.RemoteAddr); err == nil {
+		appendHostToXForwardHeader(request.Header, clientIP)
+	}
+
+	resp, err := client.Do(request)
+	if err != nil {
+		http.Error(writer, "Bad Gateway", http.StatusBadGateway)
+		errLog.Errorf("Error proxying backend: %s", strings.ReplaceAll(err.Error(), "%", "%%"))
+		return
+	}
+	defer resp.Body.Close()
+
+	delHopHeaders(resp.Header)
+
+	copyHeader(writer.Header(), resp.Header)
+	if returnCode != 0 {
+		writer.WriteHeader(returnCode)
+	} else {
+		writer.WriteHeader(resp.StatusCode)
+	}
+	io.Copy(writer, resp.Body)
+}
+
+func DefaultRequestHandler(writer http.ResponseWriter, request *http.Request) {
 
 	wait := request.URL.Query().Get("wait")
 	if wait != "" {
@@ -39,6 +113,8 @@ func RequestHandler(writer http.ResponseWriter, request *http.Request) {
 
 	}
 
+	stdLog.Printf("%s %s", request.Method, request.RequestURI)
+
 	writer.WriteHeader(returnCode)
 	writer.Write([]byte(""))
 }
@@ -46,7 +122,7 @@ func RequestHandler(writer http.ResponseWriter, request *http.Request) {
 func getEnvDefault(name string, defaultValue string) string {
 	variable := os.Getenv(name)
 	if variable == "" && defaultValue != "" {
-		log.Printf("Environment variable %s not set or empty, using default %s", name, defaultValue)
+		stdLog.Infof("Environment variable %s not set or empty, using default %s", name, defaultValue)
 		variable = defaultValue
 	}
 
@@ -60,4 +136,39 @@ func contains(arr []string, str string) bool {
 		}
 	}
 	return false
+}
+
+var hopHeaders = []string{
+	"Connection",
+	"Keep-Alive",
+	"Proxy-Authenticate",
+	"Proxy-Authorization",
+	"Te", // canonicalized version of "TE"
+	"Trailers",
+	"Transfer-Encoding",
+	"Upgrade",
+}
+
+func copyHeader(dst, src http.Header) {
+	for k, vv := range src {
+		for _, v := range vv {
+			dst.Add(k, v)
+		}
+	}
+}
+
+func delHopHeaders(header http.Header) {
+	for _, h := range hopHeaders {
+		header.Del(h)
+	}
+}
+
+func appendHostToXForwardHeader(header http.Header, host string) {
+	// If we aren't the first proxy retain prior
+	// X-Forwarded-For information as a comma+space
+	// separated list and fold multiple headers into one.
+	if prior, ok := header["X-Forwarded-For"]; ok {
+		host = strings.Join(prior, ", ") + ", " + host
+	}
+	header.Set("X-Forwarded-For", host)
 }
